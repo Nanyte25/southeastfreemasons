efaeebed98bd52b84fde081a23fec68c41b3f576
mfreer <mfreer@redhat.com>
2024-12-06 14:42:25 +0000

    Initial commit

---
+++ content/posts/your-post-name.md
@@ -0,0 +1,10 @@
+---
+title: "Your Post Title"
+date: 2024-03-19
+category: "Category"
+tags: ["tag1", "tag2"]
+description: "Your post description"
+thumbnail: "./thumbnail.png"
+---
+
+Your markdown content goes here... 
---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pathPrefix: "/gatsby-starter-apple",
+  siteMetadata: {
+    title: `South East Freemasons`,
+    description: `South East Freemasons`,
+    author: `South East Freemasons`,
+    siteUrl: `https://southeastfreemasons.org.uk/`,
+  },
+  // ... rest of the config
+} 
